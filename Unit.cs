57b75c7fcb776e339894af76ab8c622e608422ce
mehmetduranyilmaz <yilmazmehmetduran@gmail.com>
2026-02-27 10:15:27 +0300

    feat: add Unit class

---
+++ Unit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TS.MediatR
+{
+    public readonly struct Unit
+    {
+        public static readonly Unit Value = new();
+    }
+}
